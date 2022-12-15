b6df26ffa88466508966805d15d722442f4806db
Bhavini20 <singhbhavini18@gmail.com>
2022-12-15 23:24:51 +0530

    passing values

--- spring_demo_one/src/com/practice/springboot/CricketCoach.java
+++ spring_demo_one/src/com/practice/springboot/CricketCoach.java
@@ -4,10 +4,36 @@ public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	//add new fields for sending in email and team name
+	
+	private String emailAddress;
+	private String team;
+	
+	//set the setter and getter methods for injections
+	
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("Inside setter method: emailaddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("Inside setter method: setTeam ");
+		this.team = team;
+	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
 	//create a new args constructor
 	public CricketCoach() {	
 	}
 	
+
 	//create a setter method
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
