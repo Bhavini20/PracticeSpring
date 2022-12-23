8f3ff8e95c27e879fff4af6b09f2cd4a458d4e98
Bhavini20 <singhbhavini18@gmail.com>
2022-12-23 23:42:58 +0530

    spring annotation

---
+++ spring_demo annotations/src/com/practice/springboot/AnnotationDemoApp.java
@@ -0,0 +1,25 @@
+package com.practice.springboot;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+		
+		//read spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml"); 
+		
+		//get the bean
+		//Coach theCoach = context.getBean("thatSillyCoach", Coach.class);
+		
+		//get the bean using default bean id tennisCoach same name as class, first character lower case
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
+		//call method
+		System.out.println(theCoach.getDailyWorkout());
+		
+		//close
+		context.close();
+	}
+
+}
--- spring_demo annotations/src/com/practice/springboot/TennisCoach.java
+++ spring_demo annotations/src/com/practice/springboot/TennisCoach.java
@@ -2,12 +2,13 @@ package com.practice.springboot;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+//@Component("thatSillyCoach")
+//default bean id
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
 	public String getDailyWorkout() {
-		
 		return "Practice backhand volley";
 	}
 
